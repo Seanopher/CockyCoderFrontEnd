31b00362a10fccb1ce8d242ae333b63c4b511c42
Hunter <hunterhughes2787@gmail.com>
2023-12-07 00:55:39 -0500

    added NewProject, NewTask, and DeleteTask files. Still needs work

--- cocky_fx/src/main/java/cocky_fx/PrimaryController.java
+++ cocky_fx/src/main/java/cocky_fx/PrimaryController.java
@@ -16,6 +16,27 @@ public class PrimaryController {
     @FXML
     private PasswordField passwordTextField;
 
+    /* 
+    @FXML
+    protected TextField newTaskNameField;
+
+    @FXML 
+    private TextField taskDescriptionField;
+
+    @FXML
+    private TextField assignTeamMembersField;
+
+    @FXML
+    private TextField taskTypeTextField;
+
+    @FXML
+    private TextField taskToDoField;
+
+    @FXML
+    private TextField newProjectNameField;
+    */
+
+
     @FXML
     private void logInButtonClicked() {
         String username = usernameTextField.getText();
@@ -33,8 +54,29 @@ public class PrimaryController {
     }
     @FXML
     private void signUpButtonClicked() {
-            SceneManager.getInstance().showSignup();
+            SceneManager.getInstance().showNewTask();
         
     }
+    @FXML
+    private void ConfirmProjectButtonClicked() {
+            SceneManager.getInstance().showLogin();
+            //this needs to be changed later
+    }
 
+    @FXML
+    private void ConfirmTaskButtonClicked() {
+            SceneManager.getInstance().showLogin();
+            //This needs to be CHANGED later too
+    }
+    @FXML
+    private void DeleteTaskButtonClicked(){
+            SceneManager.getInstance().showDeleteTask();
+    }
+
+    //NEEDS BACKEND
+    @FXML
+    private void RealDeleteTaskButtonClicked(){
+            SceneManager.getInstance().showHomepage(); //homepage or Project page?
+        //Add code from backend here
+    }
 }
