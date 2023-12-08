893697a1988e64903bedae9b76f35486643c1a22
EmmaLee Peyton <epeyton@email.sc.edu>
2023-12-08 17:56:19 -0500

    displayColumns parameter fix

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
@@ -48,9 +50,9 @@ public class HomeController {
     
     private void handleProjectDropdownSelection() {
         String selectedOption = projectDropdownNav.getValue();
-        Project project = projectFacade.project(selectedProject);
+        Project project = projectFacade.project(selectedOption);
 
-        ArrayList<String> columns = projectFacade.displayColumns(project);
+        ArrayList<String> columns = projectFacade.displayColumns(project, null);
         taskDropdownNav.getItems().clear();
         taskDropdownNav.getItems().addAll(columns);
     }
